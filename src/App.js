f12d228fd49e1a0e32b16eedf9bbb9ba6c68eca9
saidakbar123 <smahmudxojaev@gmail.com>
2021-03-05 16:38:21 +0500

    added sidebar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store } from './store'
 import { persistStore } from 'redux-persist'
+import { useState } from 'react'
+import Content from "./components/sidebar/content";
 
 function App() {
+  const [collapsed, setCollapsed] = useState(true);
   const persistor = persistStore(store)
+
   return (
+    <div>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <div className='App'>
-          <NodeFlow />
+        <div className='App' style={{
+          gridTemplateColumns: `${collapsed ? '260px' : 0} 1fr`
+        }}>
+        <div className="sidebar" style={{ transition: '2s' }}>
+          <div className="sidebar-content-wrapper">
+            <Content />
+          </div>
+          <button className="sidebar-btn" onClick={() => {setCollapsed(prev => !prev)}}>{collapsed ? '<' : '>'}</button>
+        </div>
+          <div className="nodeflow" style={{ transition: '2s' }}><NodeFlow /></div>
         </div>
       </PersistGate>
     </Provider>
+    </div>
   )
 }
 
---
+++ src/components/sidebar/content.jsx
@@ -0,0 +1,10 @@
+export default function Rel () 
+{
+    return (
+        <div style={{ height: '100px', width: '600px', background: 'red' }}>
+            <div className="header">
+                Список вопросов
+            </div>
+        </div>
+    )
+}
